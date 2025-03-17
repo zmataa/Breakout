0c08f8a6f3c7fed792c40495ddfa8f76f5dde41b
Zane <zmatarieh@bsd220.org>
2025-03-17 09:55:48 -0500

    Initial Commit

---
+++ BreakoutTests/BreakoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreakoutTests.swift
+//  BreakoutTests
+//
+//  Created by Zane Matarieh on 3/17/25.
+//
+
+import Testing
+@testable import Breakout
+
+struct BreakoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
