9f08dd67a4bda379f064eb925eceef9670a0aaec
Zane <zmatarieh@bsd220.org>
2025-04-02 09:42:15 -0500

    created bal;

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -10,9 +10,10 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    
+    var ball = SKShapeNode()
     override func didMove(to view: SKView) {
         createBackground()
+        resetGame()
         //this stuff happens once when the app opens
     }
     func createBackground() {
@@ -28,6 +29,57 @@ class GameScene: SKScene {
             let moveForever = SKAction.repeatForever(moveLoop)
             starsBackground.run(moveForever)
         }
+    }
+    func makeBall() {
+        
+        ball.removeFromParent ()
+        // remove the ball (if it exists)
+        
+        ball = SKShapeNode(circleOfRadius: 10)
+        
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+        ball.strokeColor = .black
+        
+        ball.fillColor = .yellow
+        
+        ball.name = "ball"
+        
+        // physics shape matches ball image
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        
+        // ignores all forces and impulses
+        
+        ball.physicsBody?.isDynamic = false
+        
+        // use precise collision detection
+        
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        
+        // no loss of energy from friction
+        
+        ball.physicsBody?.friction = 0
+        
+        // gravity is not a factor
+        
+        ball.physicsBody?.affectedByGravity = false
+        
+        // bounces fully off of other objects
+        
+        ball.physicsBody?.restitution = 1
+        
+        // does not slow down over time
+        
+        ball.physicsBody?.linearDamping = 0
+        
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball)
+    }
+    func resetGame() {
+        //this stuff happens before each gaeme strarts
+        makeBall()
     }
 }
 
