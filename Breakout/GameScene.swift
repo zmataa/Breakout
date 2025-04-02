010b709b4f780a267414ba823edf8cfa30c22474
Zane <zmatarieh@bsd220.org>
2025-04-02 10:07:26 -0500

    created ball dymamic

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -8,15 +8,18 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
     
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         createBackground()
         resetGame()
+        kickBall()
         //this stuff happens once when the app opens
     }
     func createBackground() {
@@ -122,6 +125,10 @@ class GameScene: SKScene {
         makePaddle()
         makeBrick()
         makeLoseZone()
+    }
+    func kickBall() {
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
     }
 }
 
