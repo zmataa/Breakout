ac8be33cbe37928d0aff08ab949513acb299ac61
Zane <zmatarieh@bsd220.org>
2025-04-03 09:48:32 -0500

    setup gamge over routine

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -14,12 +14,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
+    var playLabel = SKLabelNode ()
+    var livesLabel = SKLabelNode()
+    var scoreLabel = SKLabelNode()
+    var playingGame = false
+    var score = 0
+    var lives = 3
+    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         createBackground()
         resetGame()
-        kickBall()
+        makeLabels()
         //this stuff happens once when the app opens
     }
     func createBackground() {
@@ -125,35 +132,83 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         makePaddle()
         makeBrick()
         makeLoseZone()
+        updateLabels()
     }
     func kickBall() {
         ball.physicsBody?.isDynamic = true
         ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
     }
+    func updateLabels() {
+        scoreLabel.text = "Score: \(score)"
+        scoreLabel.text = "Lives: \(lives)"
+    }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            paddle.position.x = location.x
+            if playingGame {
+                paddle.position.x = location.x
+            } else {
+                for node in nodes(at: location) {
+                    if node.name == "playLabel" {
+                        playingGame = true
+                        node.alpha = 0
+                        score = 0
+                        lives = 3
+                        updateLabels()
+                        kickBall()
+                    }
+                }
+            }
         }
     }
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            paddle.position.x = location.x
+            if(playingGame) {
+                paddle.position.x = location.x
+            }
         }
     }
     func didBegin(_ contact: SKPhysicsContact) {
-        
-        if contact.bodyA.node?.name == "brick" ||
-            contact.bodyB.node?.name == "brick" {
-            print ("You win!")
-            brick.removeFromParent()
-            ball.removeFromParent()
+        if contact.bodyA.node?.name == "brick" || contact.bodyB.node?.name == "brick" {
+            gameOver(winner: true)
         }
-        if contact.bodyA.node?.name == "loseZone" ||
-            contact.bodyB.node?.name == "loseZone" {
-            print("You lose!")
-            ball.removeFromParent()
+        if contact.bodyA.node?.name == "loseZone" || contact.bodyB.node?.name == "loseZone" {
+            gameOver(winner: false)
         }
     }
+
+    func makeLabels() {
+        playLabel.fontSize = 24
+        playLabel.text = "Tap to start"
+        playLabel.fontName = "Arial"
+        playLabel.position = CGPoint(x: frame.midX, y: frame.midY - 50)
+        playLabel.name = "playLabel"
+        addChild(playLabel)
+
+        livesLabel.fontSize = 18
+        livesLabel.fontColor = .black
+        livesLabel.fontName = "Arial"
+        livesLabel.position = CGPoint(x: frame.minX + 50, y: frame.minY + 18)
+        addChild(livesLabel)
+
+        scoreLabel.fontSize = 18
+        scoreLabel.fontColor = .black
+        scoreLabel.fontName = "Arial"
+        scoreLabel.position = CGPoint(x: frame.maxX - 50, y: frame.minY + 18)
+        addChild(scoreLabel)
+    }
+    func gameOver(winner: Bool) {
+        playingGame = false
+        playLabel.alpha = 1
+        resetGame()
+        if winner {
+            playLabel.text = "You win! Tap to play again"
+        } else {
+            playLabel.text = "You lose! Tap to play again"
+        }
+    }
+
+
 }
