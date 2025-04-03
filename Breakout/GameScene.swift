33986345b073ba472f3307f6d2c5c89945cd00ed
Zane <zmatarieh@bsd220.org>
2025-04-03 09:33:57 -0500

    Determine winners and losers

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -142,6 +142,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             paddle.position.x = location.x
         }
     }
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        if contact.bodyA.node?.name == "brick" ||
+            contact.bodyB.node?.name == "brick" {
+            print ("You win!")
+            brick.removeFromParent()
+            ball.removeFromParent()
+        }
+        if contact.bodyA.node?.name == "loseZone" ||
+            contact.bodyB.node?.name == "loseZone" {
+            print("You lose!")
+            ball.removeFromParent()
+        }
+    }
 }
-
-
