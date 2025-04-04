8b43eebc693bcb1e0d514e90350421ce51ca9af6
Zane <zmatarieh@bsd220.org>
2025-04-04 09:31:58 -0500

    setup rows of multicolor bricks

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -62,14 +62,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         }
         bricks.removeAll()  // clear the array
         removedBricks = 0   // reset the counter
-
+        
         // now, figure the number and spacing of each row of bricks
-        let count = Int(frame.width) / 55  // bricks per row
+        let count = Int(frame.width) / 55    // bricks per row
         let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int(frame.minX) + 25
-        let y = Int(frame.maxY) - 65
-        for i in 0..<count {
-            let x = i * 55 + xOffset
-            makeBrick(x: x, y: y, color: .green)
+        let colors: [UIColor] = [.blue, .orange, .green]
+        for r in 0..<3 {
+            let y = Int(frame.maxY) - 65 - (r * 25)
+            for i in 0..<count {
+                let x = i * 55 + xOffset
+                makeBrick(x: x, y: y, color: colors[r])
+            }
         }
     }
 
@@ -195,13 +198,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
             if contact.bodyA.node == brick || contact.bodyB.node == brick {
                 score += 1
                 updateLabels()
-                brick.removeFromParent()
-                removedBricks += 1
-                if removedBricks == bricks.count {
-                    gameOver(winner: true)
+                if brick.color == .blue {
+                    brick.color = .orange     // blue bricks turn orange
+                }
+                else if brick.color == .orange {
+                    brick.color = .green      // orange bricks turn green
+                }
+                else {  // must be a green brick, which get removed
+                    brick.removeFromParent()
+                    removedBricks += 1
+                    if removedBricks == bricks.count {
+                        gameOver(winner: true)
+                    }
                 }
             }
         }
+
 
         if contact.bodyA.node?.name == "loseZone" ||
            contact.bodyB.node?.name == "loseZone" {
